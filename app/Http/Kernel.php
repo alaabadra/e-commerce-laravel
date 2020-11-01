344ac06b9339ff66fb4a2e7bf2d7c3688abd0a7b
alaabadra <alaabadra4@gmail.com>
2020-11-01 06:58:03 +0200

    edit in file kernel to put middleware for admin auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
         're_admin' =>\App\Http\Middleware\RedirectIfAuthenticatedAdmin::class, 
         're_user' =>\App\Http\Middleware\RedirectIfAuthenticated::class, 
         'auth' => \App\Http\Middleware\Authenticate::class,
+        // 'admin' => \App\Http\Middleware\AuthenticateAmin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
