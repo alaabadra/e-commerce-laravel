8b95575a49e7e68f8924d688f6c260ce8081506f
alaabadra <alaabadra4@gmail.com>
2020-11-01 05:40:13 +0200

    add mapAdminRoutes to make file admin.php in folder routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,8 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
-
+    public const HOME = '/user';
+    public const ADMIN = '/admin/dashboard';
     /**
      * Define your route model bindings, pattern filters, etc.
      *
@@ -59,7 +59,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('web')
+        Route::prefix('user')
+        ->middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
     }
