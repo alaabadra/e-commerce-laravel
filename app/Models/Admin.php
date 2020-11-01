51989db202304963da2a9e9ac5cdaf9b153e7256
alaabadra <alaabadra4@gmail.com>
2020-11-01 05:49:03 +0200

    add model for admin and change in file user model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    //
+    protected $guard='admin';
+    use Notifiable;
+
+    protected $table='admins';
+    protected $fillable = [
+        'name', 'email', 'password','created_at','updated_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
