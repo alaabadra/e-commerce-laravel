7c4c5663cd21d46e51c7dc8e91b149acf41cf28e
alaabadra <alaabadra4@gmail.com>
2020-11-01 06:57:05 +0200

    add file login for admin

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(){
+        return view('admin.auth.login');
+    }
+
+    public function loginPost(LoginRequest $req){
+            $remember_me=$req->has('remember_me')? true :false;//if req has this name remember_me (check on checkbox) 
+            if(Auth::guard('admin')->attempt(['email'=>$req->input('email'),'password'=>$req->input('password')])){
+                return  redirect('admin/dasboard');    
+            }
+             return redirect()->back()->with(['errors'=>'there is some errors']);
+
+    }
+
+    public function logout(){
+        if(Auth::guard('admin')->check()){
+            Auth::guard('admin')->logout();
+            return  redirect('admin/login');    
+        }else{
+            return redirect()->back()->with(['errors'=>'you havent auth in this rout: you havnt session admin to logout on it']);
+        }
+
+    }
+    
+}
