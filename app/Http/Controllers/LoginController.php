dfeec7e8fdc08e0b2a0a6d6fdd919d66f1e28f65
alaabadra <alaabadra4@gmail.com>
2020-11-01 05:49:36 +0200

    add controller for user

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,24 +1,52 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\LoginRequest;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+
     public function getLogin(){
-        return view('admin.auth.login');
+        return view('auth.login');
     }
 
-    public function login(LoginRequest $req){
+    public function loginPost(LoginRequest $req){
         $remember_me=$req->has('remember_me')? true :false;//if req has this name remember_me (check on checkbox) 
-        if(auth()->guard('admin')->attempt(['email'=>$req->input('email'),'password'=>$req->input('password')])){
-
-            return redirect()->route('admin.dashboard');
+        if(auth()->guard('web')->attempt(['email'=>$req->input('email'),'password'=>$req->input('password')])){
+            return redirect()->route('user.home');
         }
-
          return redirect()->back()->with(['errors'=>'هناك خطا بالبيانات']);
 
     }
-    
+
+    public function reg(){
+        return view('auth.register');
+    }
+
+    public function regPost(Request $req){
+        $data=$req->all();
+        $userCount=User::where('email',$data['email'])->count();
+        if($userCount>0){
+            return redirect('/user/login')->with('flash_message_error','email is exist already , so you can make  login direct');
+        }else{
+          if($data['password_confirmation']==$data['password']){
+            User::insert(['name'=>$data['name'],'email'=>$data['email'],'password'=>bcrypt($data['password'])]);
+            return  redirect('/user');    
+          }else{
+            return  redirect('/user/reg');    
+          }
+        }
+    }
+
+    public function logout(){
+        if(Auth::check()){
+            Auth::logout();//delete from guard
+            return  redirect('user/login');
+        }else{
+            return redirect()->back()->with(['errors'=>'you havent auth in this rout: you havnt session user to logout on it']);
+        }
+    }
 }
