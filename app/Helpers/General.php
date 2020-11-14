336c2217a67dd67b8485b152597337d89c87b4d0
alaabadra <alaabadra4@gmail.com>
2020-11-14 02:27:48 +0200

    updated in file general.php in folder helper

--- app/Helpers/General.php
+++ app/Helpers/General.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Models\Language;
 function uploadImage($folder,$image){
+  // dd($image);
     $image->store('/',$folder);
     $filename=$image->hasName();
     $path='images/'.$folder.'/'.$filename;
@@ -8,18 +9,15 @@ function uploadImage($folder,$image){
   }
 
 function forDefaultLang(){
+  // dd(00);
   $localLang= Config::get('app.locale');
+  // dd($localLang);
     $langDefaultInTableLangCount=Language::where(['language_abbr'=>$localLang])->count();
     if($langDefaultInTableLangCount!==0){//the default lang is exist in table language
         $langDefaultInTableLang=Language::where(['language_abbr'=>$localLang])->first();//get default language from table languages
         return $langDefaultInTableLang;
     }else{
-      $routeViewDashboard='admin/dashboard/view';
-      return response()->json([
-          'status'=>500,
-          'message'=>'There is something wrong, please try again, because this website not contain on the default language , pls click here to generate it'.$routeViewDashboard.'after that you can return into this route'
-      ]);
-
+      return  null;     
   }
 }
 
