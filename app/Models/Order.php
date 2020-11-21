0f289aa6bb042ea46ca17289d6845760b89aae45
alaabadra <alaabadra4@gmail.com>
2020-11-21 06:31:47 +0200

    edit in models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,14 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    public function similarProducts(){
-        return $this->hasMany("App\Models\SimilarProduct");
+    public function product(){
+        return $this->belongsTo("App\Models\Product");
     }
     public function Couponcodes(){
         return $this->hasMany("App\Models\Couponcode");
-    }
-    public function productAttributes(){
-        return $this->hasMany("App\Models\ProductAttribute");
     }
     public function delivery(){
         return $this->belongsTo("App\Models\Delivery");
@@ -22,6 +19,6 @@ class Order extends Model
         return $this->belongsTo("App\Models\User");
     }
     public function productsOrders(){
-        return $this->belongsToMany("App\Models\ProductAttributs",'order_product_attrs','order_id','product_attr_id');
+        return $this->belongsToMany("App\Models\Products",'order_products','order_id','product_id');
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,19 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    public function productAttributes(){
-        return $this->hasMany("App\Models\ProductAttribute");
-    }
+
     public function category(){
         return $this->belongsTo("App\Models\Category");
     }
-    public function similarProducts(){
-        return $this->hasMany("App\Models\SimilarProduct");
-    }
+
     public function ordersProducts(){
-        return $this->belongsToMany("App\Models\ProductAttributs",'orders_products','product_id','order_id');
+        return $this->belongsToMany("App\Models\Products",'orders_products','product_id','order_id');
     }
     public function languages(){
         return $this->hasMany("App\Models\Language");
+    }
+    public function order(){
+        return $this->belongsTo("App\Models\Order");
     }
 }
